7f46ecd3182a842135465de97b1563169feec5cc
Dr.Abc <me@drabc.net>
2025-01-17 23:29:18 +0800

    use curl instead of steamapi

--- Plugins/SCModelDownloader/UtilHTTPClient.cpp
+++ Plugins/SCModelDownloader/UtilHTTPClient.cpp
@@ -1,68 +1,155 @@
+#include <vector>
+#include <future>
+#include <thread>
+
 #include <metahook.h>
 #include "plugins.h"
+
+#include "libcurl.h"
 #include "UtilHTTPClient.h"
 
-static HINTERFACEMODULE g_hUtilHTTPClient;
-static IUtilHTTPClient* g_pUtilHTTPClient;
-
-IUtilHTTPClient* UtilHTTPClient()
-{
-	return g_pUtilHTTPClient;
-}
+static std::vector<std::future<CHTTPRespond*>> s_aryPromise{};
 
 void UtilHTTPClient_Init()
 {
-	g_hUtilHTTPClient = Sys_LoadModule("UtilHTTPClient_SteamAPI.dll");
-
-	if (!g_hUtilHTTPClient)
-	{
-		g_pMetaHookAPI->SysError("Could not load UtilHTTPClient_SteamAPI.dll");
-		return;
-	}
-
-	auto factory = Sys_GetFactory(g_hUtilHTTPClient);
-
-	if (!factory)
-	{
-		g_pMetaHookAPI->SysError("Could not get factory from UtilHTTPClient_SteamAPI.dll");
-		return;
-	}
-
-	g_pUtilHTTPClient = (decltype(g_pUtilHTTPClient))factory(UTIL_HTTPCLIENT_STEAMAPI_INTERFACE_VERSION, NULL);
-
-	if (!g_pUtilHTTPClient)
-	{
-		g_pMetaHookAPI->SysError("Could not get UtilHTTPClient from UtilHTTPClient_SteamAPI.dll");
-		return;
-	}
-
-	if (g_pUtilHTTPClient)
-	{
-		CUtilHTTPClientCreationContext context;
-		g_pUtilHTTPClient->Init(&context);
-	}
+	CURL_Init();
 }
 
 void UtilHTTPClient_RunFrame()
 {
-	if (g_pUtilHTTPClient)
+	for (auto iter = s_aryPromise.begin(); iter != s_aryPromise.end();)
 	{
-		g_pUtilHTTPClient->RunFrame();
+		auto status = iter->wait_for(std::chrono::seconds(0));
+		if (status == std::future_status::ready)
+		{
+			auto rep = iter->get();
+			if (rep)
+			{
+				auto fnContinue = rep->m_pfnContinueWith;
+				(*fnContinue)(rep, rep->m_pUser);
+			}
+			delete rep;
+			iter = s_aryPromise.erase(iter);
+		}
+		else
+			iter++;
 	}
 }
 
 void UtilHTTPClient_Shutdown()
 {
-	if (g_pUtilHTTPClient)
-	{
-		g_pUtilHTTPClient->Shutdown();
-		g_pUtilHTTPClient->Destroy();
-		g_pUtilHTTPClient = nullptr;
-	}
+	CURL_Shutdown();
+}
 
-	if (g_hUtilHTTPClient)
-	{
-		Sys_FreeModule(g_hUtilHTTPClient);
-		g_hUtilHTTPClient = NULL;
-	}
+
+
+CHTTPPayload::~CHTTPPayload()
+{
+	if (m_pData)
+		delete[] m_pData;
+}
+
+CHTTPPayload::CHTTPPayload(const char* d, size_t l)
+{
+	m_iSize = l;
+	m_pData = new char[m_iSize];
+	memcpy(m_pData, d, m_iSize);
+
+}
+
+CHTTPRequest* CHTTPRequest::Create(const char* url)
+{
+	CHTTPRequest* req = new CHTTPRequest();
+	req->m_szUrl = url;
+	return req;
+}
+
+CHTTPRequest* CHTTPRequest::OnFailed(const fnHTTPRequestFailed& pfn, void* user)
+{
+	this->m_pfnOnFailed = &pfn;
+	this->m_pFailedUserData = user;
+	return this;
+}
+
+CHTTPRequest* CHTTPRequest::OnResponed(const fnHTTPRequestCompleted& pfn, void* user)
+{
+	this->m_pfnOnCompleted = &pfn;
+	this->m_pCompletedUserData = user;
+	return this;
+}
+
+CHTTPRequest* CHTTPRequest::ContinueWith(const fnHTTPContinueWith& pfn, void* user)
+{
+	this->m_pfnContinueWith = &pfn;
+	this->m_pContinueUserData = user;
+	return this;
+}
+
+static size_t append(void* ptr, size_t size, size_t nmemb, void* user) {
+	std::vector<char>* p = (std::vector<char>*)user;
+	auto cs = p->size();
+	p->resize(cs + size * nmemb);
+	memcpy(p->data() + cs, ptr, size * nmemb);
+	return size * nmemb;
+}
+
+void CHTTPRequest::Start()
+{
+	auto future = std::async([](CHTTPRequest* req) -> CHTTPRespond* {
+		void* curl = curl_easy_init();
+		if (curl)
+		{
+			std::vector<char> readBuffer;
+			curl_easy_setopt(curl, CURLOPT_URL, req->m_szUrl.c_str());
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, append);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+			CURLcode res = curl_easy_perform(curl);
+			if (res != CURLE_OK)
+			{
+				auto fnFailed = req->m_pfnOnFailed;
+				if (fnFailed)
+					(*fnFailed)(req->m_pFailedUserData);
+				curl_easy_cleanup(curl);
+				delete req;
+				return nullptr;
+			}
+			else
+			{
+				long http_code = 0;
+				curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+				CHTTPPayload* payload = new CHTTPPayload(readBuffer.data(), readBuffer.size());
+				auto fnComplete = req->m_pfnOnCompleted;
+				if (fnComplete)
+				{
+					(*fnComplete)(payload, req->m_pCompletedUserData);
+				}
+				curl_easy_cleanup(curl);
+				CHTTPRespond* respond = new CHTTPRespond(payload, (int)http_code);
+				respond->m_pfnContinueWith = req->m_pfnContinueWith;
+				respond->m_pUser = req->m_pContinueUserData;
+				delete req;
+				return respond;
+			}
+		}
+		else
+		{
+			auto fnFailed = req->m_pfnOnFailed;
+			if (fnFailed)
+				(*fnFailed)(req->m_pFailedUserData);
+			return nullptr;
+		}
+		}, this);
+	s_aryPromise.push_back(future);
+}
+
+CHTTPRespond::~CHTTPRespond()
+{
+	if (m_pPayload)
+		delete m_pPayload;
+}
+
+CHTTPRespond::CHTTPRespond(CHTTPPayload* payload, int code)
+{
+	m_pPayload = payload;
+	m_iHTTPCode = code;
 }
--- Plugins/SCModelDownloader/UtilHTTPClient.h
+++ Plugins/SCModelDownloader/UtilHTTPClient.h
@@ -1,9 +1,62 @@
 #pragma once
 
-#include <IUtilHTTPClient.h>
+#include <string>
+
+class CHTTPPayload
+{
+public:
+	~CHTTPPayload();
+
+	CHTTPPayload(const char* d, size_t l);
+	char* m_pData = nullptr;
+	size_t m_iSize = 0;
+};
+
+typedef void (*fnHTTPContinueWith)(class CHTTPRespond* payload, void* user);
+
+class CHTTPRespond
+{
+public:
+	~CHTTPRespond();
+
+	CHTTPRespond(CHTTPPayload* payload, int code);
+
+	int m_iHTTPCode = -1;
+	CHTTPPayload* m_pPayload = nullptr;
+
+	const fnHTTPContinueWith* m_pfnContinueWith = nullptr;
+	void* m_pUser = nullptr;
+};
+
+typedef void (*fnHTTPRequestFailed)(void* user);
+typedef void (*fnHTTPRequestCompleted)(CHTTPPayload* payload, void* user);
+
+class CHTTPRequest
+{
+public:
+	static CHTTPRequest* Create(const char* url);
+
+	CHTTPRequest* OnFailed(const fnHTTPRequestFailed& pfn, void* user);
+	CHTTPRequest* OnResponed(const fnHTTPRequestCompleted& pfn, void* user);
+
+	//in main thread
+	CHTTPRequest* ContinueWith(const fnHTTPContinueWith& pfn, void* user);
+
+	void Start();
+
+
+	const fnHTTPRequestCompleted* m_pfnOnCompleted = nullptr;
+	void* m_pCompletedUserData = nullptr;
+
+	const fnHTTPRequestFailed* m_pfnOnFailed = nullptr;
+	void* m_pFailedUserData = nullptr;
+
+	const fnHTTPContinueWith* m_pfnContinueWith = nullptr;
+	void* m_pContinueUserData = nullptr;
+
+	std::string m_szUrl;
+};
 
 void UtilHTTPClient_Init();
 void UtilHTTPClient_RunFrame();
 void UtilHTTPClient_Shutdown();
-
-IUtilHTTPClient* UtilHTTPClient();
